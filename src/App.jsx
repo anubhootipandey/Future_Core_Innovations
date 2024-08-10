788a4e412e5422f751cda3f76f15a218a9ffdfca
rohan <panditrohan738@gmail.com>
2024-08-10 23:02:05 +0530

    complete blog page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import About from './components/AboutUS/About';
 import Service from './components/Services/Service';
+import Features from './components/Features/Features';
+import Blog from './components/Blog/Blog';
 
 
 
@@ -16,6 +18,8 @@ const App = () => {
         <Routes>
           <Route exact path='/about' element={<About />} />
           <Route exact path='/service' element={<Service />} />
+          <Route exact path='/feature' element={<Features />} />
+          <Route exact path='/blog' element={<Blog/>} />
         </Routes>
         <Footer />
       </BrowserRouter>
